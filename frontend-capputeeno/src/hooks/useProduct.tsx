e4bc7308cdd66041f5b1f524381c42bf86a074e9
Everton Reis <evertoontcarmo@gmail.com>
2023-09-10 10:19:02 -0300

    Create useProduct hook

---
+++ frontend-capputeeno/src/hooks/useProduct.tsx
@@ -0,0 +1,28 @@
+import { useQuery } from "react-query";
+import { fetchDataProduct } from "@/modules/resource";
+
+export function useProduct(productId: string) {
+  const query = `query {
+      Product(id: "${productId}") {
+        name
+        description
+        image_url
+        price_in_cents
+        category
+      }
+    }
+  `;
+
+  const { data, isLoading, isError } = useQuery({
+    queryFn: () => fetchDataProduct(query),
+    queryKey: ["product", productId],
+    enabled: !!productId,
+    staleTime: 1000 * 60 * 5,
+  });
+
+  return {
+    data: data?.data.Product,
+    isLoading,
+    isError,
+  };
+}

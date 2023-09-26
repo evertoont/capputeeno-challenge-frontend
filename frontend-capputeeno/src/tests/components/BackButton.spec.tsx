5cb53a9df6264b9fc994734329035d53cee09ddf
Everton Reis <evertoontcarmo@gmail.com>
2023-09-26 20:13:49 -0300

    Create backButton test

---
+++ frontend-capputeeno/src/tests/components/BackButton.spec.tsx
@@ -0,0 +1,43 @@
+import { ThemeProvider } from "styled-components";
+import { BackButton } from "../../components/BackButton";
+import { fireEvent, render } from "@testing-library/react";
+import { theme } from "../../app/theme";
+
+const mockFunction = jest.fn();
+
+jest.mock("next/navigation", () => {
+  return {
+    useRouter() {
+      return {
+        back: mockFunction,
+      };
+    },
+  };
+});
+
+describe("BackButton", () => {
+  it("should render backButton correctly", () => {
+    const { getByText, getByRole } = render(
+      <ThemeProvider theme={theme}>
+        <BackButton />
+      </ThemeProvider>
+    );
+
+    expect(getByRole("button")).toBeInTheDocument();
+    expect(getByText("Voltar")).toBeInTheDocument();
+  });
+
+  it("should call navigation back function", () => {
+    const { getByRole } = render(
+      <ThemeProvider theme={theme}>
+        <BackButton />
+      </ThemeProvider>
+    );
+
+    const button = getByRole("button");
+
+    fireEvent.click(button, "click");
+
+    expect(mockFunction).toHaveBeenCalled();
+  });
+});

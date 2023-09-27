88414ee71679a01209d4748ddd8dbc1a78d063fe
Everton Reis <evertoontcarmo@gmail.com>
2023-09-26 22:24:17 -0300

    Change name folder for test

--- frontend-capputeeno/jest.config.ts
+++ frontend-capputeeno/jest.config.ts
@@ -140,7 +140,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ["<rootDir>/src/tests/setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/test/setup.ts"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
